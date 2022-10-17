98097c44a4905b5b497eb5d2d9dfa9dc5764db4c
evgeniydolgov <amyr214519@gmail.com>
2022-10-17 23:06:31 +0300

    funish task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+  constructor (direct) {
+    this.direct = true;
+    if (direct === false) {
+      this.direct = false;
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw Error('Incorrect arguments!');
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let encryptedMessage = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].charCodeAt() > 64 && message[i].charCodeAt() < 91) {
+        let encryptedLetterIndex = ((message[i].charCodeAt() - 65) + (key[keyIndex % key.length].charCodeAt() - 65)) % 26;
+        keyIndex++;
+        encryptedMessage = encryptedMessage + String.fromCharCode(encryptedLetterIndex + 65);
+      } else {
+        encryptedMessage = encryptedMessage + message[i];
+      }
+    }
+
+    if (this.direct === true) {
+      return encryptedMessage;
+    } else {
+      return encryptedMessage.split('').reverse().join('');
+    }
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw Error('Incorrect arguments!');
+
+    key = key.toUpperCase();
+    let message = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if (encryptedMessage[i].charCodeAt() > 64 && encryptedMessage[i].charCodeAt() < 91) {
+        let letterIndex = ((encryptedMessage[i].charCodeAt() - 65) - (key[keyIndex % key.length].charCodeAt() - 65) +26) % 26;
+        keyIndex++;
+        message = message + String.fromCharCode(letterIndex + 65);
+      } else {
+        message = message + encryptedMessage[i];
+      }
+    }
+
+    if (this.direct === true) {
+      return message;
+    } else {
+      return message.split('').reverse().join('');
+    }
   }
 }
 
